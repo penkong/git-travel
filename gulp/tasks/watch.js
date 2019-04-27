1bd0ca3d8bb08d5e2875c7485f209c982451e4bc
mazdak <nazemi.work@gmail.com>
2019-04-27 06:29:49 -0700

    browsersync gulp architecture and error handling on gulp

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create();
+// ========================
+
+
+gulp.task('watch', function () {
+  // automate serve us watch app folder from distance of gulp.js
+  // can add other setting in init method
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  watch('./app/index.html', function () {
+    // watch and refresh page on save
+    browserSync.reload();
+  })
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('cssInject');
+  })
+});
+
+// =============================
+
+//inject css from posted css file to index that watched by browserSync.
+//styles task is dependency inside arry = new Arg (run before other and first complete it)
+gulp.task('cssInject', ['styles'], function () {
+  return gulp.src('./app/temp/styles/styles.css')
+    //stream method make everything we pipe in available in browser
+    .pipe(browserSync.stream());
+});
+
+
+
+
+
+
+
+
+// gulp.task('default', function () {
+//   console.log("gooray gulp task");
+// });
+// // 
+// gulp.task('html', function () {
+//   console.log("imagine sth to html");
+// });
