f53cb19f448c7d71356e4b710409607e8d48980a
mazdak <nazemi.works@gmail.com>
2019-05-07 22:25:25 -0700

    reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,10 @@
 import MobileMenu from './module/MobileMenu';
 
+import RevealOnScroll from './module/RevealOnScroll';
+import $ from 'jquery';
 
 
 
 const mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "65%");
